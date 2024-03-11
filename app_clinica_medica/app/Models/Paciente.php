c6187885af6637b0a487331d5f0087ca3552b7bf
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-11 17:25:23 +0000

    Update Paciente.php
    Requisitos de preenchimento para a inserção de dados na tabela pacientes.

--- app_clinica_medica/app/Models/Paciente.php
+++ app_clinica_medica/app/Models/Paciente.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Paciente extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome_paciente',
+        'genero',
+        'morada',
+        'nif',
+        'data_nascimento',
+        'telemovel',
+        'email',
+    ];
+
+
 }
