37cf928cc547e8f86240001806ddb286e86baf84
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-11 18:40:42 +0000

    Update 2024_03_11_183733_create_agendas_table.php
    Criada a tabela da Agenda na base de dados . Com migração.

--- app_clinica_medica/database/migrations/2024_03_11_183733_create_agendas_table.php
+++ app_clinica_medica/database/migrations/2024_03_11_183733_create_agendas_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('agendas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_medicos');
+            $table->foreign('id_medicos')->references('id')->on('medicos')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('id_consultas');
+            $table->foreign('id_consultas')->references('id')->on('consultas')->onDelete('cascade')->onUpdate('cascade');
+            $table->date('data_agenda');
+            $table->time('hora_agenda');
+            $table->enum('status_agenda', ['Agendada', 'Cancelada', 'Realizada']);
             $table->timestamps();
         });
     }
