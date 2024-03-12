dbb1c488c45de03b8d2ec3eb72ba4a394c6ea1db
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 12:20:52 +0000

    Update PacienteController.php
    Criar e guardar registos no método store

--- app_clinica_medica/app/Http/Controllers/PacienteController.php
+++ app_clinica_medica/app/Http/Controllers/PacienteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Paciente;
 use Illuminate\Http\Request;
 
 class PacienteController extends Controller
@@ -19,7 +20,8 @@ class PacienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $paciente = Paciente::create($request->all());
+        return response()->json($paciente, 201);
     }
 
     /**
