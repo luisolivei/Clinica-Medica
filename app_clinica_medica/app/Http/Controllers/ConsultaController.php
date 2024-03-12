43ec233c2b9bd9d063aa1084c703c455c0696950
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 15:33:20 +0000

    Update ConsultaController.php
    Criando o método show. Mostrar consultas com parâmetro id.

--- app_clinica_medica/app/Http/Controllers/ConsultaController.php
+++ app_clinica_medica/app/Http/Controllers/ConsultaController.php
@@ -28,9 +28,9 @@ class ConsultaController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Consulta $consulta)
     {
-        //
+        return response()->json($consulta);
     }
 
     /**
