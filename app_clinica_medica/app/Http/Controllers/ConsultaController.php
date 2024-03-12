0afb06b700e2b78c5e10ee4a1132bc22324da32e
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 16:55:59 +0000

    Update ConsultaController.php
    Deixamos de utilizar o método estático e passamos a usar o método de um objeto.

--- app_clinica_medica/app/Http/Controllers/ConsultaController.php
+++ app_clinica_medica/app/Http/Controllers/ConsultaController.php
@@ -18,7 +18,8 @@ class ConsultaController extends Controller
      */
     public function index()
     {
-        $consultas = Consulta::all();
+        // $consultas = Consulta::all();
+        $consultas = $this->consulta->all();
         return response()->json($consultas);
     }
 
