8461b4dcbcc97e5327ae01d151d63a5481d7dcb2
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 12:08:17 +0000

    Update api.php
    criação das rotas da api

--- app_clinica_medica/routes/api.php
+++ app_clinica_medica/routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\MedicoController;
+use App\Http\Controllers\ConsultaController;
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\MedicamentoController;
+use App\Http\Controllers\EspecialidadeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +27,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('/pacientes', PacienteController::class);
+Route::apiResource('/medicos', MedicoController::class);
+Route::apiResource('/medicamento', MedicamentoController::class);
+Route::apiResource('/especialidades', EspecialidadeController::class);
+Route::apiResource('/agenda', AgendaController::class);
+Route::apiResource('/consultas', ConsultaController::class);
