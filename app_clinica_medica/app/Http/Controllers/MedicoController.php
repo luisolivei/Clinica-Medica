8fe1cc9e726854d20d875b56653b7d99fa4f207b
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 16:40:37 +0000

    Update MedicoController.php
    Criar o atributo medico, recebendo o objeto $medico.

--- app_clinica_medica/app/Http/Controllers/MedicoController.php
+++ app_clinica_medica/app/Http/Controllers/MedicoController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class MedicoController extends Controller
 {
+    protected $medico;
+    public function __construct(Medico $medico)
+    {
+        $this->medico = $medico;
+    }
     /**
      * Display a listing of the resource.
      */
