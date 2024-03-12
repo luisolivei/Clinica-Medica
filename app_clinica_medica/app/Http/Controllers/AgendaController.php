baabb74f9209fe623b0f5f095b9128bf3e68a526
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 15:33:54 +0000

    Update AgendaController.php
    Criando o método show. Mostrar agendas com parâmetro id.

--- app_clinica_medica/app/Http/Controllers/AgendaController.php
+++ app_clinica_medica/app/Http/Controllers/AgendaController.php
@@ -28,9 +28,9 @@ class AgendaController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Agenda $agenda)
     {
-        //
+        return response()->json($agenda);
     }
 
     /**
