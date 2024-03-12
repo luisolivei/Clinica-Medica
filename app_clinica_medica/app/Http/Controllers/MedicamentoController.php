c35122a327131ad12baf258aa006b9478c83f4e6
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 15:40:29 +0000

    Update MedicamentoController.php
    Criação da atualização dos registos dos medicamentos. método PUT ou PATCH.

--- app_clinica_medica/app/Http/Controllers/MedicamentoController.php
+++ app_clinica_medica/app/Http/Controllers/MedicamentoController.php
@@ -36,9 +36,10 @@ class MedicamentoController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Medicamento $medicamento)
     {
-        //
+        $medicamento->update($request->all());
+        return response()->json($medicamento, 200);
     }
 
     /**
