1bfaab6c290327fa84771fba28f4dd8a40c0223f
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-12 12:19:03 +0000

    Update MedicamentoController.php
    Criar e guardar registos no método store

--- app_clinica_medica/app/Http/Controllers/MedicamentoController.php
+++ app_clinica_medica/app/Http/Controllers/MedicamentoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Medicamento;
 use Illuminate\Http\Request;
 
 class MedicamentoController extends Controller
@@ -19,7 +20,8 @@ class MedicamentoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $medicamento = Medicamento::create($request->all());
+        return response()->json($medicamento, 201);
     }
 
     /**
