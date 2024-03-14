440919d014bc68d15a6a5a9a21b6e391c923d27b
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-03-14 16:30:47 +0000

    Update AuthController.php
    Implementação do login e logout

--- app_clinica_medica/app/Http/Controllers/AuthController.php
+++ app_clinica_medica/app/Http/Controllers/AuthController.php
@@ -23,6 +23,55 @@ class AuthController extends Controller
             'email' => $fields['email'],
             'password' => bcrypt($fields['password'])
         ]);
-        return response ($user, 201);
+
+        $token = $user->createToken($request->nameToken)->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
     }
+
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('UserLogin')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+
+
+    }
+
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => 'Logged out'
+        ] , 200);
+        ;
+    }
+
 }
